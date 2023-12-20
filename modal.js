d187d7b5b1bc4e7b9e36d647bd1db1a53ca7fb1b
Mehdi Boutab <mehdiboutab@gmail.com>
2023-12-20 15:53:49 +0100

    Added close button functionality to modal

--- modal.js
+++ modal.js
@@ -12,12 +12,22 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Select the close button inside the modal
+const closeModalBtn = document.querySelector(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// CLose modal event
+
+closeModalBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
